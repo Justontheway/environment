3b94b8460ca1fd27fbb37cbae472ecafd8370db0
NOTHING <wcxontheway@126.com>
2018-11-14 12:05:02 +0800

    add bash centots

---
+++ base_centos.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+yum -y install tree ctags \
+    python-setuptools \
+    git \
+    gcc \
+    gcc-c++ \
+    python-setuptools \
+    python-devel \
+    freetype \
+    freetype-devel \
+    libpng \
+    libpng-devel \
+    zlib
+
