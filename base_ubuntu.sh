459b9f557c1b93381677d48ede45d3e037346354
NOTHING <wcxontheway@126.com>
2019-04-15 14:06:51 +0800

    add env for ubuntu

---
+++ base_ubuntu.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+apt-get install -y tree ctags \
+    python-setuptools \
+    git \
+    gcc \
+    g++ \
+    python-setuptools \
+    python-dev \
+    libfreetype6 \
+    libfreetype6-dev \
+    libpng3 \
+    libpng3-dev \
+    libreadline6 zlib1g openssl bzip2 libbz2-1.0 sqlite3 \
+    libreadline6-dev zlib1g-dev libssl-dev libbz2-dev libsqlite3-dev
+
+
