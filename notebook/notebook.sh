8dd5c07aa13506231d162be9c2fac7d40a1653ab
NOTHING <justontheway@yeah.net>
2022-10-11 17:48:25 +0800

    notebook启动: 修改日志路径

--- notebook/notebook.sh
+++ notebook/notebook.sh
@@ -5,9 +5,11 @@
 #
 
 cd $(dirname $0)
+TODAY=$(date +%Y_%m_%d)
+LOGFILE=notebook_${TODAY}.log
 
 mkdir -p ~/.notebooks
 mkdir -p ~/.notebooks/dataset
 export DATASET_DIRECTORY=~/.notebooks/dataset
-nohup python -m notebook --config notebook-config --allow-root > notebook.log 2>&1 &
-tail -f notebook.log
+nohup python -m notebook --config notebook-config --allow-root > ${LOGFILE} 2>&1 &
+tail -f ${LOGFILE}
